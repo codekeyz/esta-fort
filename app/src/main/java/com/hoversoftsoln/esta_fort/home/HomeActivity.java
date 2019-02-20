e391b7cbbb72615f3daa8fd4849033f630da99b0
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-20 11:57:38 +0000

    Added Splash Boilerplate & logic

--- app/src/main/java/com/hoversoftsoln/esta_fort/home/HomeActivity.java
+++ app/src/main/java/com/hoversoftsoln/esta_fort/home/HomeActivity.java
@@ -8,15 +8,21 @@ import android.view.MenuItem;
 
 import com.hoversoftsoln.esta_fort.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class HomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
 
+        setSupportActionBar(toolbar);
     }
 
     @Override
