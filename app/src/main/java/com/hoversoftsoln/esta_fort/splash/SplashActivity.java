a3ebe6a7d753938aeaa73be6d9d66943b0620d51
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-20 13:28:31 +0000

    Added Profile BoilerPlate

--- app/src/main/java/com/hoversoftsoln/esta_fort/splash/SplashActivity.java
+++ app/src/main/java/com/hoversoftsoln/esta_fort/splash/SplashActivity.java
@@ -32,6 +32,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
         ButterKnife.bind(this);
 
         // View model
