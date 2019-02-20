3a478437b5bce70965204a33da09a48171e2e9bf
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-20 03:10:38 +0000

    Added FirebaseConfig Boilerplate

--- app/src/main/java/com/hoversoftsoln/esta_fort/EstaFort.java
+++ app/src/main/java/com/hoversoftsoln/esta_fort/EstaFort.java
@@ -2,6 +2,9 @@ package com.hoversoftsoln.esta_fort;
 
 import android.app.Application;
 
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
 import timber.log.Timber;
 
 import static timber.log.Timber.DebugTree;
@@ -13,6 +16,18 @@ public class EstaFort extends Application {
         super.onCreate();
 
         if (BuildConfig.DEBUG) {
+
+            // Create a Remote Config Setting to enable developer mode, which you can use to increase
+            // the number of fetches available per hour during development. See Best Practices in the
+            // README for more information.
+            // [START enable_dev_mode]
+            FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                    .setDeveloperModeEnabled(BuildConfig.DEBUG)
+                    .build();
+            FirebaseRemoteConfig.getInstance().setConfigSettings(configSettings);
+
+            FirebaseRemoteConfig.getInstance().setDefaults(R.xml.remote_config_defaults);
+
             Timber.plant(new DebugTree());
         }
     }
