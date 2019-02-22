cee308de0563d3c34c51bf40bc6d1cb18a3ae415
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-22 11:26:51 +0000

    Added DriverActivity & ViewModel

--- app/src/main/java/com/hoversoftsoln/esta_fort/booking/DriverActivity.java
+++ app/src/main/java/com/hoversoftsoln/esta_fort/booking/DriverActivity.java
@@ -1,4 +1,93 @@
 package com.hoversoftsoln.esta_fort.booking;
 
-public class DriverActivity {
+import android.app.ProgressDialog;
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.hoversoftsoln.esta_fort.R;
+import com.hoversoftsoln.esta_fort.home.DriverAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class DriverActivity extends AppCompatActivity {
+
+    @BindView(R.id.refreshLayout)
+    SwipeRefreshLayout refreshLayout;
+
+    @BindView(R.id.recycler_drivers)
+    RecyclerView driverRv;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    private DriverActivityViewModel driverActivityViewModel;
+    private DriverAdapter driverAdapter;
+    private ProgressDialog dialog;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        dialog = new ProgressDialog(this);
+        dialog.setTitle("Placing Request");
+        dialog.setMessage("Please wait while your request is placed");
+
+        driverActivityViewModel = ViewModelProviders.of(this).get(DriverActivityViewModel.class);
+        init();
+
+        driverActivityViewModel.loadingService().observe(this, loading -> refreshLayout.setRefreshing(loading));
+
+        driverActivityViewModel.getDrivers().observe(this, drivers -> this.driverAdapter.setDriverList(drivers));
+
+        driverActivityViewModel.requestService().observe(this, data -> {
+            if (data.first) {
+                if (!dialog.isShowing()){
+                    dialog.show();
+                }
+
+                if (data.second != null) {
+                    if (data.second.isSuccessful()) {
+                        Toast.makeText(this, "Your Request has been placed.", Toast.LENGTH_SHORT).show();
+                    }else {
+                        Toast.makeText(this, "An error occurred. Try again later", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+    private void init() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        refreshLayout.setOnRefreshListener(() -> driverActivityViewModel.getDrivers().observe(this, drivers -> this.driverAdapter.setDriverList(drivers)));
+
+        driverRv.setHasFixedSize(true);
+        driverRv.setLayoutManager(new LinearLayoutManager(this));
+
+        driverAdapter = new DriverAdapter(this);
+        driverAdapter.setHasStableIds(true);
+        driverRv.setAdapter(driverAdapter);
+
+        driverAdapter.setOnDriverClick(driver -> driverActivityViewModel.onDriverClick(DriverActivity.this, driver));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/hoversoftsoln/esta_fort/booking/DriverActivityViewModel.java
+++ app/src/main/java/com/hoversoftsoln/esta_fort/booking/DriverActivityViewModel.java
@@ -1,4 +1,122 @@
 package com.hoversoftsoln.esta_fort.booking;
 
-public class DriverActivityViewModel {
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.util.Pair;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.Query;
+import com.hoversoftsoln.esta_fort.core.data.Driver;
+import com.hoversoftsoln.esta_fort.core.data.Request;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+public class DriverActivityViewModel extends ViewModel {
+
+    private MutableLiveData<List<Driver>> drivers;
+    private MutableLiveData<Boolean> loadingService;
+    private MutableLiveData<Pair<Boolean, Task>> requestService;
+    private AlertDialog dialog;
+    private Query driversCollection;
+    private CollectionReference requestsCollection;
+    private ListenerRegistration driversRegistration;
+
+    public DriverActivityViewModel() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        this.driversCollection = db.collection("Drivers").whereEqualTo("status", 1);
+        this.requestsCollection = db.collection( "Requests");
+    }
+
+    public LiveData<List<Driver>> getDrivers() {
+        if (drivers == null) {
+            drivers = new MutableLiveData<>();
+            if (loadingService == null) {
+                loadingService = new MutableLiveData<>();
+            }
+            loadDrivers();
+        }
+        return drivers;
+    }
+
+    public LiveData<Boolean> loadingService() {
+        if (loadingService == null) {
+            loadingService = new MutableLiveData<>();
+        }
+        return loadingService;
+    }
+
+    public LiveData<Pair<Boolean, Task>> requestService() {
+        if (requestService == null) {
+            requestService = new MutableLiveData<>();
+        }
+        return requestService;
+    }
+
+    private void loadDrivers() {
+        this.loadingService.postValue(true);
+        List<Driver> driverList = new ArrayList<>();
+        this.driversRegistration = this.driversCollection.addSnapshotListener((qdocs, e) -> {
+            this.loadingService.postValue(false);
+            if (qdocs != null) {
+                driverList.clear();
+                for (DocumentSnapshot d:
+                        qdocs.getDocuments()) {
+                    Driver driver = d.toObject(Driver.class);
+                    if (driver != null) {
+                        driver.setId(d.getId());
+                        driverList.add(driver);
+                    }
+                }
+                this.drivers.postValue(driverList);
+            }
+        });
+    }
+
+    private void sendRequest(Request request) {
+        this.requestService.postValue(new Pair<>(true, null));
+        request.setDateCreated(new Date().getTime());
+        requestsCollection.add(request).addOnCompleteListener(task -> requestService.postValue(new Pair<>(false, task)));
+    }
+
+    @Override
+    protected void onCleared() {
+        if (this.driversRegistration != null) {
+            this.driversRegistration.remove();
+        }
+    }
+
+    void onDriverClick(Activity context, Driver driver) {
+        if (dialog == null) {
+           dialog = new AlertDialog.Builder(context)
+                    .setTitle("Request Shuttle")
+                    .setMessage("You are requesting a shuttle. " + driver.getUsername())
+                    .setPositiveButton("Proceed", (dialog, which) -> {
+                        Request request = new Request();
+                        request.setUserID(FirebaseAuth.getInstance().getCurrentUser().getUid());
+                        request.setDriverID(driver.getId());
+                        request.setDateCreated(new Date().getTime());
+                        request.setStatus(0);
+                        sendRequest(request);
+                    })
+                    .setNegativeButton("Cancel", (dialog, which) -> dialog.cancel())
+                    .create();
+        }
+
+
+    }
 }
