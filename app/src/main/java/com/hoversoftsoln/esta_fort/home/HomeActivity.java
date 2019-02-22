c71c286af0f91d9db55b74059864342107cca6e5
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-22 00:39:21 +0000

    Refactored HomeViewMOdel & implemented in HomeActivity

--- app/src/main/java/com/hoversoftsoln/esta_fort/home/HomeActivity.java
+++ app/src/main/java/com/hoversoftsoln/esta_fort/home/HomeActivity.java
@@ -1,20 +1,33 @@
 package com.hoversoftsoln.esta_fort.home;
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.hoversoftsoln.esta_fort.R;
+import com.hoversoftsoln.esta_fort.core.data.EstaUser;
+import com.hoversoftsoln.esta_fort.splash.SplashViewModel;
+import com.hoversoftsoln.esta_fort.utils.FabMenuAdapter;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import timber.log.Timber;
+import uk.co.markormesher.android_fab.FloatingActionButton;
 
 public class HomeActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+
+    @BindView(R.id.fab_menu)
+    FloatingActionButton fabLayout;
+
+    private HomeViewModel homeViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +35,23 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
+
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
+
+        EstaUser estaUser = new EstaUser();
+        estaUser.setUsername("Lomotey Edwin");
+        estaUser.setEmail("edwin@gmail.com");
+        estaUser.setTelephone("0244961291");
+
+        fabLayout.setContentCoverEnabled(true);
+        fabLayout.setContentCoverColour(Color.argb(80, 0, 0, 0));
+        fabLayout.setSpeedDialMenuAdapter(homeViewModel.getFabMenuAdapter());
+
+        homeViewModel.getServices().observe(this, data ->
+                Toast.makeText(this, data.get(0).getTitle(), Toast.LENGTH_SHORT).show());
+
+        homeViewModel.loadingService().observe(this, res -> Timber.d(res  ? "Yes Loading": "Not Loading"));
+
     }
 
     @Override
--- app/src/main/java/com/hoversoftsoln/esta_fort/home/HomeViewModel.java
+++ app/src/main/java/com/hoversoftsoln/esta_fort/home/HomeViewModel.java
@@ -1,4 +1,76 @@
 package com.hoversoftsoln.esta_fort.home;
 
-public class HomeViewModel {
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.hoversoftsoln.esta_fort.core.data.Service;
+import com.hoversoftsoln.esta_fort.utils.FabMenuAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<List<Service>> services;
+    private MutableLiveData<Boolean> loadingService;
+
+    private FabMenuAdapter fabMenuAdapter;
+    private CollectionReference servicesCollection;
+    private ListenerRegistration servicesRegistration;
+
+    public HomeViewModel() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        this.servicesCollection = db.collection("Services");
+        fabMenuAdapter = new FabMenuAdapter();
+    }
+
+    public LiveData<List<Service>> getServices() {
+        if (services == null) {
+            services = new MutableLiveData<>();
+            if (loadingService == null) {
+                loadingService = new MutableLiveData<>();
+            }
+            loadServices();
+        }
+        return services;
+    }
+
+    public LiveData<Boolean> loadingService() {
+        if (loadingService == null) {
+            loadingService = new MutableLiveData<>();
+        }
+        return loadingService;
+    }
+
+    private void loadServices() {
+        this.loadingService.postValue(true);
+        List<Service> serviceList = new ArrayList<>();
+        this.servicesRegistration = this.servicesCollection.addSnapshotListener((queryDocumentSnapshots, e) -> {
+            this.loadingService.postValue(false);
+            if (queryDocumentSnapshots != null) {
+                for (DocumentSnapshot d :
+                        queryDocumentSnapshots.getDocuments()) {
+                    Service s = d.toObject(Service.class);
+                    serviceList.add(s);
+                }
+                this.services.postValue(serviceList);
+            }
+        });
+    }
+
+    public FabMenuAdapter getFabMenuAdapter() {
+        return fabMenuAdapter;
+    }
+
+    @Override
+    protected void onCleared() {
+        if (this.servicesRegistration != null) {
+            this.servicesRegistration.remove();
+        }
+    }
 }
