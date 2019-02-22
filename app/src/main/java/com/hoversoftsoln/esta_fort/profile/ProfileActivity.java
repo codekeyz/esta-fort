e2fa8059738e3afc96be339a86887b306d7810ee
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-22 05:04:55 +0000

    Fixed ProfileActivity & viewMOdel

--- app/src/main/java/com/hoversoftsoln/esta_fort/profile/ProfileActivity.java
+++ app/src/main/java/com/hoversoftsoln/esta_fort/profile/ProfileActivity.java
@@ -1,19 +1,143 @@
 package com.hoversoftsoln.esta_fort.profile;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.hoversoftsoln.esta_fort.R;
+import com.hoversoftsoln.esta_fort.data.EstaUser;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
+import me.zhanghai.android.materialprogressbar.MaterialProgressBar;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    @BindView(R.id.profile_image)
+    CircleImageView profileImage;
+    @BindView(R.id.username)
+    TextView username;
+    @BindView(R.id.userEmail)
+    TextView userEmail;
+    @BindView(R.id.userTelephone)
+    TextView userTelephone;
+    @BindView(R.id.userLocation)
+    TextView userLocation;
+    @BindView(R.id.etTelephone)
+    EditText etTelephone;
+    @BindView(R.id.etUsername)
+    EditText etUsername;
+    @BindView(R.id.etEmail)
+    EditText etEmail;
+    @BindView(R.id.etLocation)
+    EditText etLocation;
+    @BindView(R.id.updateProfile)
+    Button updateProfile;
+    @BindView(R.id.saveProfile)
+    Button saveProfile;
+    private ProfileViewModel profileViewModel;
+
+    @BindView(R.id.editLayout)
+    LinearLayout editLayout;
+
+    @BindView(R.id.dataLayout)
+    LinearLayout dataLayout;
+
+    @BindView(R.id.loading)
+    MaterialProgressBar loader;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         ButterKnife.bind(this);
+
+        profileViewModel = ViewModelProviders.of(this).get(ProfileViewModel.class);
+
+        setDefaults();
+
+        profileViewModel.loadingService().observe(this, loading -> {
+            if (loading) {
+                updateProfile.setVisibility(View.GONE);
+                loader.setVisibility(View.VISIBLE);
+                editLayout.setVisibility(View.GONE);
+                dataLayout.setVisibility(View.GONE);
+            }else {
+                updateProfile.setVisibility(View.VISIBLE);
+                loader.setVisibility(View.GONE);
+                dataLayout.setVisibility(View.VISIBLE);
+            }
+        });
+
+        profileViewModel.getUserAccount().observe(this, user -> {
+            if (user != null) {
+                editLayout.setVisibility(View.GONE);
+                dataLayout.setVisibility(View.VISIBLE);
+                this.username.setText(user.getUsername());
+                this.userEmail.setText(user.getEmail());
+                this.userTelephone.setText(user.getTelephone());
+                this.userLocation.setText(user.getLocation());
+            }else {
+                editLayout.setVisibility(View.VISIBLE);
+            }
+        });
+
+        profileViewModel.viewState().observe(this, this::switchState);
+
+        this.updateProfile.setOnClickListener(v -> this.switchState(0));
+
+        this.saveProfile.setOnClickListener(v -> {
+            EstaUser estaUser = new EstaUser();
+            estaUser.setEmail(etEmail.getText().toString());
+            estaUser.setUsername(etUsername.getText().toString());
+            estaUser.setLocation(etLocation.getText().toString());
+            estaUser.setTelephone(etTelephone.getText().toString());
+            this.profileViewModel.updateUserAccount(estaUser);
+        });
     }
+
+    private void setDefaults() {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+        if (user != null) {
+
+            if (user.getDisplayName() != null) {
+                this.username.setText(user.getDisplayName());
+                this.etUsername.setText(user.getDisplayName());
+            }
+
+            if (user.getEmail() != null) {
+                this.userEmail.setText(user.getEmail());
+                this.etEmail.setText(user.getEmail());
+            }
+
+            if (user.getPhoneNumber() != null) {
+                this.userTelephone.setText(user.getPhoneNumber());
+                this.etTelephone.setText(user.getPhoneNumber());
+            }
+        }
+    }
+
+    public void switchState(int state) {
+        switch (state) {
+            case 0:
+                dataLayout.setVisibility(View.GONE);
+                editLayout.setVisibility(View.VISIBLE);
+                break;
+            case 1:
+                dataLayout.setVisibility(View.VISIBLE);
+                editLayout.setVisibility(View.GONE);
+                break;
+        }
+    }
+
 }
--- app/src/main/java/com/hoversoftsoln/esta_fort/profile/ProfileViewModel.java
+++ app/src/main/java/com/hoversoftsoln/esta_fort/profile/ProfileViewModel.java
@@ -1,4 +1,99 @@
 package com.hoversoftsoln.esta_fort.profile;
 
-public class ProfileViewModel {
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+import android.support.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.SetOptions;
+import com.hoversoftsoln.esta_fort.data.EstaUser;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProfileViewModel extends ViewModel {
+
+    private MutableLiveData<EstaUser> estaUser;
+    private MutableLiveData<Boolean> loadingService;
+    private MutableLiveData<Integer> state;
+
+    private DocumentReference userRefence;
+    private ListenerRegistration userDocListenerReg;
+
+    public ProfileViewModel() {
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        this.userRefence = db.collection("Users").document(uid);
+    }
+
+    public LiveData<EstaUser> getUserAccount() {
+        if (estaUser == null) {
+            estaUser = new MutableLiveData<>();
+            if (loadingService == null) {
+                loadingService = new MutableLiveData<>();
+            }
+            if (state == null) {
+                state = new MutableLiveData<>();
+            }
+            loadEstaUser();
+        }
+        return estaUser;
+    }
+
+    public LiveData<Boolean> loadingService() {
+        if (loadingService == null) {
+            loadingService = new MutableLiveData<>();
+        }
+        return loadingService;
+    }
+
+    public LiveData<Integer> viewState() {
+        if (state == null) {
+            state = new MutableLiveData<>();
+        }
+        return state;
+    }
+
+    private void loadEstaUser() {
+        this.loadingService.postValue(true);
+        this.userDocListenerReg = this.userRefence.addSnapshotListener((documentSnapshot, e) -> {
+            this.loadingService.postValue(false);
+            if (documentSnapshot != null) {
+                EstaUser user= documentSnapshot.toObject(EstaUser.class);
+                this.estaUser.postValue(user);
+                this.state.postValue(1);
+            }else {
+                this.state.postValue(0);
+            }
+        });
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        this.userDocListenerReg.remove();
+    }
+
+    public void updateUserAccount(EstaUser estaUser) {
+        this.loadingService.postValue(true);
+        Map<String, Object> datamap = new HashMap<>();
+        datamap.put("username", estaUser.getUsername());
+        datamap.put("email", estaUser.getEmail());
+        datamap.put("telephone", estaUser.getTelephone());
+        datamap.put("location", estaUser.getLocation());
+        userRefence.set(datamap, SetOptions.merge()).addOnCompleteListener(task -> {
+            this.loadingService.postValue(false);
+            if (task.isSuccessful()){
+                this.estaUser.postValue(estaUser);
+            }else {
+                this.state.postValue(0);
+            }
+        });
+    }
 }
